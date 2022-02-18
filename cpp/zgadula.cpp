fe2a6746641e65f2b74ad12e9f902b4829267717
info1do8 <99181926+info1do8@users.noreply.github.com>
2022-02-18 23:40:59 +0100

    Update zgadula.cpp
    po testach z Filipem

--- cpp/zgadula.cpp
+++ cpp/zgadula.cpp
@@ -4,18 +4,24 @@
 
 using namespace std;
 
-int cel, i, liczba;
+int cel, i=1, liczba;
 int main()
 {
-    srand(time(NULL)); //pobiera liczbę sekund od 1970r
+    srand(time(NULL)); //pobiera liczbê sekund od 1970r
     cel= rand()%100+1; //reszta z dzielenia to 0-99, ale +1
-    cout << "Zgadnij jaka to liczba od 1 do 100..." << endl;
-    cout<<"strzelaj!" << endl;
-    cin>> liczba;
-    if (liczba<1||liczba>100) cout <<"Wybierz z podanego zakresu (1-100)"<<endl;
-    else {
-    cout<<"czy to liczba "<< liczba << "?"<<endl;
-    cout<<"nie, to " << cel << " :)"<< endl;
-    }
+    cout << "Zgaduj zgadula, jaka to liczba?" << endl;
+    cout<<"Strzelaj! ";
+        while (liczba!=cel) {
+            if (liczba<1||liczba>100) cout <<"Wybierz z podanego zakresu (1-100)"<<endl;
+            else {
+                    if (liczba>cel) cout <<"za duzo, strzelaj dalej: "<<endl;
+                    else cout <<"za malo, strzelaj dalej: "<<endl;
+                i++;
+            }
+            cin>> liczba;
+        }
+    cout<<"TAK!!! Udalo Ci sie zgadn¹æ po "<<i<<" probach."<<endl;
+    //system("pause"); //stopuje, gdy .cpp uruchamiamy
+
     return 0;
 }
